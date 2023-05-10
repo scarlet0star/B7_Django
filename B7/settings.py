d0357f7173fec020394ff13d078f3f47aef541e3
Lee Hyeonsik <hyeon5819@gmail.com>
2023-05-10 19:58:44 +0900

    hot fix

--- B7/settings.py
+++ B7/settings.py
@@ -115,7 +115,7 @@ STATIC_URL = "static/"
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
 
-# AUTH_USER_MODEL = 'user.User'
+AUTH_USER_MODEL = 'users.User'
 
 REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': (
