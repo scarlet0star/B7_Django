f6b205d09224469ff386746adfff8cdf5bb64138
scarlet0star <scarlet0star@gmail.com>
2023-05-14 05:28:50 +0900

    Revert "modify user view"
    This reverts commit d9369b161bf1df6f7791dd4deb08dbcbe44e0c6e.

--- users/views.py
+++ users/views.py
@@ -10,25 +10,19 @@ from users.serializers import UserSerializer, UserDetailSerializer, UserChangeSe
 class MyTokenObtainPairView(TokenObtainPairView):
     serializer_class = MyTokenObtainPairSerializer
 # 로그아웃
-
-
 class Logout(APIView):
     def post(self, request, format=None):
         request.user.auth_token.delete()
         return Response(status=status.HTTP_200_OK)
-# 회원가입
-
-
+# 회원가입    
 class SignUp(APIView):
     def post(self, request, format=None):
         serializer = UserSerializer(data=request.data)
         if serializer.is_valid():
             serializer.save()
             return Response(data={'message': '회원가입 성공'}, status=status.HTTP_201_CREATED)
-        return Response(data={'message': serializer.error_messages + '회원가입 실패'}, status=status.HTTP_400_BAD_REQUEST)
-# 회원정보 수정
-
-
+        return Response(data={'message': '회원가입 실패'}, status=status.HTTP_400_BAD_REQUEST)
+# 회원정보 수정    
 class Update(APIView):
     def put(self, request, format=None):
         serializer = UserDetailSerializer(request.user, data=request.data)
@@ -36,51 +30,31 @@ class Update(APIView):
             serializer.save()
             return Response(data={'message': '회원정보 수정 성공'}, status=status.HTTP_200_OK)
         return Response(data={'message': '회원정보 수정 실패'}, status=status.HTTP_400_BAD_REQUEST)
-
-    def patch(self, request, *args, **kwargs):
-        serializer = UserDetailSerializer(
-            request.user, data=request.data, partial=True)
-
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data, status=status.HTTP_200_OK)
-
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-
-
-# 회원상세정보
+# 회원상세정보    
 class Detail(APIView):
     def get(self, request, format=None):
         serializer = UserDetailSerializer(request.user)
         return Response(data=serializer.data, status=status.HTTP_200_OK)
-# 회원탈퇴
-
-
+# 회원탈퇴    
 class Delete(APIView):
     def delete(self, request, format=None):
         serializer = UserDeleteSerializer(request.user)
         request.user.delete()
         return Response(data={'message': '회원탈퇴 성공'}, status=status.HTTP_200_OK)
-# 비밀번호 찾기
-
-
+# 비밀번호 찾기    
 class FindPassword(APIView):
     def get(self, request, format=None):
         serializer = UserFindSerializer(request.user)
         return Response(data=serializer.data, status=status.HTTP_200_OK)
-# 비밀번호 변경
-
-
+# 비밀번호 변경    
 class ChangePassword(APIView):
-    def patch(self, request, format=None):
-        serializer = UserChangeSerializer(request.user, data=request.data, partial=True)
+    def put(self, request, format=None):
+        serializer = UserChangeSerializer(request.user, data=request.data)
         if serializer.is_valid():
             serializer.save()
             return Response(data={'message': '비밀번호 변경 성공'}, status=status.HTTP_200_OK)
-        return Response(data={'message': serializer.errors }, status=status.HTTP_400_BAD_REQUEST)
-# 회원목록
-
-
+        return Response(data={'message': '비밀번호 변경 실패'}, status=status.HTTP_400_BAD_REQUEST)
+# 회원목록    
 class List(APIView):
     def get(self, request, format=None):
         users = User.objects.all()
