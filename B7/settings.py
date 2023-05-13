1d4fcc92a654e553f791beae4ecff29eacf83d34
Lee Hyeonsik <hyeon5819@gmail.com>
2023-05-14 05:46:12 +0900

    now

--- B7/settings.py
+++ B7/settings.py
@@ -83,19 +83,26 @@ CHANNEL_LAYERS = {
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.mysql',
+#         'NAME': os.getenv('MYSQL_DATABASE'),
+#         'USER': os.getenv('MYSQL_USER'),
+#         'PASSWORD': os.getenv('MYSQL_PASSWORD'),
+#         'HOST': 'mysql',
+#         'PORT': 3306,
+#         'OPTIONS':{
+#             'charset':'utf8mb4'
+#         }
+#     }
+# }
+
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': os.getenv('MYSQL_DATABASE'),
-        'USER': os.getenv('MYSQL_USER'),
-        'PASSWORD': os.getenv('MYSQL_PASSWORD'),
-        'HOST': 'mysql',
-        'PORT': 3306,
-        'OPTIONS':{
-            'charset':'utf8mb4'
-        }
-    }
+"default": {
+"ENGINE": "django.db.backends.sqlite3",
+"NAME": BASE_DIR / "db.sqlite3",
 }
+} 
 
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
