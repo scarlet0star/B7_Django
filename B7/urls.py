9caf1a187f01dabadcfec99a0362c1e379564074
scarlet0star <scarlet0star@gmail.com>
2023-05-15 04:00:43 +0900

    error fix

--- B7/urls.py
+++ B7/urls.py
@@ -41,7 +41,7 @@ urlpatterns = [
     path("articles/", include("articles.urls")),
     path("office/", include("office.urls")),
     path("chat/", include("chat.urls")),
-    path("users/", include("users.urls")),
+    path("user/", include("users.urls")),
     re_path(r'^swagger(?P<format>\.json|\.yaml)$', schema_view.without_ui(cache_timeout=0), name='schema-json'),
     re_path(r'^swagger/$', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
     re_path(r'^redoc/$', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
